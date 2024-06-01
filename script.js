5e39aeedd5d7cbb32118b9741e2ddbf3a6e88f0b
bknsehan <sechanridho.h@gmail.com>
2024-06-01 10:36:10 +0700

    Update 0.7

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-const tags = document.querySelectorAll('.tag');
+document.addEventListener("DOMContentLoaded", function() {
+    const searchBar = document.getElementById("searchBar");
+    const texts = [":v", ":)", ":(", ":|"];
+    let index = 0;
 
-tags.forEach(tag => {
-    tag.addEventListener('click', () => {
-        const currentState = tag.getAttribute('data-state');
-        if (currentState === 'unselected') {
-            tag.setAttribute('data-state', 'selected');
-            tag.classList.add('selected');
-        } else if (currentState === 'selected') {
-            tag.setAttribute('data-state', 'excluded');
-            tag.classList.remove('selected');
-            tag.classList.add('excluded');
-        } else {
-            tag.setAttribute('data-state', 'unselected');
-            tag.classList.remove('excluded');
-        }
-    });
+    setInterval(() => {
+        searchBar.placeholder = texts[index];
+        index = (index + 1) % texts.length;
+    }, 2000); // Ganti teks setiap 2 detik
 });
