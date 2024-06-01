13adf9bbbe95d3d912ed0ebf311f425e305d1970
bknsehan <sechanridho.h@gmail.com>
2024-06-01 10:38:36 +0700

    Update 0.8

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const searchBar = document.getElementById("searchBar");
-    const texts = [":v", ":)", ":(", ":|"];
+    const texts = [":v", ":)", ":(", ":|", ":3"]; // Menambahkan ":3" ke dalam array
     let index = 0;
 
     setInterval(() => {
         searchBar.placeholder = texts[index];
         index = (index + 1) % texts.length;
-    }, 2000); // Ganti teks setiap 2 detik
+    }, 500); // Mengubah interval menjadi setiap 0.5 detik
 });
