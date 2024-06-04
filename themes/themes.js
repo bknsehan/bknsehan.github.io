3bcd98df1e50ef81364abbd566f2d0cc4e8b4b7a
bknsehan <sechanridho.h@gmail.com>
2024-06-04 20:43:17 +0700

    Update 0.21

--- themes/themes.js
+++ themes/themes.js
@@ -1,22 +1,41 @@
-// Function to change the theme
-function changeTheme(theme) {
+// Function to toggle between dark and light themes
+function toggleTheme() {
+    const currentTheme = document.body.classList.contains('dark') ? 'dark' : 'light';
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  
+    setTheme(newTheme);
+  }
+  
+  // Function to set a specific theme
+  function setTheme(theme) {
     // Remove any existing theme classes from the body
     document.body.className = '';
-  
-    // Add the new theme class to the body
     document.body.classList.add(theme);
   
-    // Save the theme to localStorage
+    // Save the new theme to localStorage
     localStorage.setItem('selectedTheme', theme);
+  
+    // Update the button text and icon
+    updateThemeToggleButton(theme);
+  }
+  
+  // Function to update the toggle button text and icon
+  function updateThemeToggleButton(theme) {
+    const themeToggleButton = document.getElementById('theme-toggle');
+    if (theme === 'dark') {
+      themeToggleButton.innerHTML = '<i class="fa-regular fa-lightbulb"></i>';
+    } else if (theme === 'light') {
+      themeToggleButton.innerHTML = '<i class="fa-solid fa-lightbulb"></i>';
+    }
   }
   
   // Load the saved theme from localStorage
   document.addEventListener('DOMContentLoaded', () => {
     const savedTheme = localStorage.getItem('selectedTheme');
-    if (savedTheme) {
-      document.body.classList.add(savedTheme);
-    } else {
-      document.body.classList.add('dark'); // Default theme
-    }
+    const theme = savedTheme ? savedTheme : 'light';
+    document.body.classList.add(theme);
+  
+    // Update the button text and icon
+    updateThemeToggleButton(theme);
   });
   
